119014dead608c64d5ecbb1603e14681404168e8
babel <cholodecki@gmail.com>
2023-10-15 19:35:53 +0200

    prepared for presentation at uni

--- assets.py
+++ assets.py
@@ -6,4 +6,4 @@ pygame.font.init()
 test = pygame.image.load("button.png")
 
 font1 = pygame.font.SysFont("Arial", 60)
-font2 = pygame.font.SysFont("Calibri", 60)
+font2 = pygame.font.SysFont("Comic Sans", 60)
--- calculator.py
+++ calculator.py
@@ -7,17 +7,17 @@ import assets
 
 
 def button_handler(button_id):
-    if button_id == 1:
-        test_text.change_texture(assets.font2, "HI", (0, 0, 0))
+    if button_id == 2:
+        test_text.change_texture(assets.font2, "HEJ", (0, 0, 0))
 
-    elif button_id == 2:
+    elif button_id == 1:
         test_text.is_visible = not test_text.is_visible
 
 
 window = window.Window(1280, 720)
 
-test_button_1 = ui_elements.Button(window, 300, 300, assets.test, 1)
-test_button_2 = ui_elements.Button(window, 400, 400, assets.test, 2)
+test_button_1 = ui_elements.Button(window, 302, 0, assets.test, 1)
+test_button_2 = ui_elements.Button(window, 302, 32, assets.test, 2)
 
 test_text = ui_elements.Text(window, 500, 500, assets.font1, "HELLO", (0, 0, 0), False)
 
