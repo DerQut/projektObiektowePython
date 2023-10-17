8afb44feacd72b794c2a86bd793eac1586ed0b35
babel <cholodecki@gmail.com>
2023-10-17 16:02:04 +0200

    fixed the fps cap

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ if __name__ == "__main__":
 
     import calculator
 
+    clock = pygame.time.Clock()
+
     while calculator.calculator_window.running:
 
-        clock = pygame.time.Clock()
         clock.tick(150)
 
         calculator.calculator_window.run()
